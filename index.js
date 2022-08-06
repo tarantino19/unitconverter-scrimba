52df3d62f2cc46d8b2654330b8e4a98d57dbb49d
tarantino19 <tolentinored19@gmail.com>
2022-08-06 19:26:16 +0800

    project done

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-`use strict`;
+"use strict";
 
-let num = 0;
-let convertThisNumber = document.getElementById("convert-this-number");
-let length = document.getElementById("length");
-let volume = document.getElementById("volume");
-let mass = document.getElementById("mass");
+let numEl = document.getElementById("num-el");
+let convertBtn = document.getElementById("convert-btn");
+let lengthEl = document.getElementById("length");
+let volumeEl = document.getElementById("volume");
+let massEl = document.getElementById("mass");
+
+console.log(convertBtn);
+
+//make the button work when clicked
+
+let inputNumber = numEl.value;
+
+convertBtn.addEventListener("click", function () {
+  inputNumber = numEl.value;
+  convert();
+});
+
+// create function to calculate
+
+const oneMeter = 3.281;
+const oneLiter = 0.264;
+const oneKg = 2.204;
+
+function calLength() {
+  let sumMeter = inputNumber / oneMeter;
+  sumMeter = parseFloat(sumMeter.toFixed(3));
+  let sumFeet = inputNumber * oneMeter;
+  sumFeet = parseFloat(sumFeet.toFixed(3));
+  lengthEl.innerHTML = `<p>${inputNumber} meters = ${sumFeet} feet | ${inputNumber} feet = ${sumMeter} meters</p>`;
+}
+
+function calVolume() {
+  let sumLiters = inputNumber / oneLiter;
+  sumLiters = parseFloat(sumLiters.toFixed(3));
+  let sumGallons = inputNumber * oneLiter;
+  sumGallons = parseFloat(sumGallons.toFixed(3));
+  volumeEl.innerHTML = `<p>${inputNumber} liters = ${sumGallons} gallons | ${inputNumber} gallons = ${sumLiters} liters</p>`;
+}
+
+function calMass() {
+  let sumKg = inputNumber / oneKg;
+  sumKg = parseFloat(sumKg.toFixed(3));
+  let sumPounds = inputNumber * oneKg;
+  sumPounds = parseFloat(sumPounds.toFixed(3));
+  massEl.innerHTML = `<p>${inputNumber} kilos = ${sumPounds} pounds | ${inputNumber} pounds = ${sumKg} kilos</p>`;
+}
+
+function convert() {
+  calLength();
+  calVolume();
+  calMass();
+}
