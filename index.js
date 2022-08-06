dc5d235e7bfa807d6cb5a042ecddaf200e93ec83
tarantino19 <tolentinored19@gmail.com>
2022-08-06 16:53:20 +0800

    html css added

---
+++ index.js
@@ -0,0 +1,7 @@
+`use strict`;
+
+let num = 0;
+let convertThisNumber = document.getElementById("convert-this-number");
+let length = document.getElementById("length");
+let volume = document.getElementById("volume");
+let mass = document.getElementById("mass");
